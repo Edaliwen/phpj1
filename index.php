f62669020b988dfc08bb2a495dfac259c8f2c5f6
Edaliwen <amandine.valtat@gmail.com>
2022-12-06 16:42:51 +0100

    Update index.php
    Enoncé exercice 9

--- index.php
+++ index.php
@@ -283,7 +283,6 @@
                         echo "Vous êtes mineur.";
                     }
                 ?>
-    </main>
         <hr>
                 <h3>Exercice 7</h3>
                 <p>Créer une variable isEasy de type boolean et l'initialiser avec une valeur.<br>
@@ -333,7 +332,7 @@
                 "Vous êtes un homme/une femme/non-binaire et vous êtes majeur/mineur."<br>
                 Gérer tous les cas.</p>
                 <?php
-                    $age = 12;
+                    $age = 18;
                     $gender = "non";
                     if($age >= 18){
                         if($gender == "homme"){
@@ -363,18 +362,40 @@
                             }else{
                                 echo "Vous êtes un homme et vous êtes mineur.<br>";
                             }
-                        break;
-                        case "femme":
-                            if($age >= 18){
-                                echo "Vous êtes une femme et vous êtes majeure.<br>";
-                            }else{
-                                echo "Vous êtes une femme et vous êtes mineure.<br>";
-                            }
-                        break;
-                        default:
-                        echo "Vous ne rentrez pas dans les cases :) <br>";
-                    }                 
-                ?>
+                            break;
+                            case "femme":
+                                if($age >= 18){
+                                    echo "Vous êtes une femme et vous êtes majeure.<br>";
+                                }else{
+                                    echo "Vous êtes une femme et vous êtes mineure.<br>";
+                                }
+                                break;
+                                default:
+                                echo "Vous ne rentrez pas dans les cases :) <br>";
+                            }                 
+                            ?>
+        <hr>
+                <h3>Exercice 9</h3>
+                <!-- à mettre en forme
+-------------------------------------------------------------------------------------------------           
+                <p>L'échelle de Richter est un outil de mesure qui permet de définir la magnitude de moment d'un tremblement de terre. Cette échelle va de 1 à 9.  
+Créer une variable **magnitude**. Selon la valeur de **magnitude**, afficher la phrase correspondante.  
+
+Magnitude   |   Phrase
+------      |    ---
+1           |   Micro-séisme impossible à ressentir.
+2           |   Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.
+3           |   Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.
+4           |   Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.
+5           |   Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.
+6           |   Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.  
+7           |   Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.
+8           |   Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.
+9           |   Séisme capable de tout détruire sur une très vaste zone.  
+
+Gérer tous les cas.  
+*Utiliser autre chose que des if else*</p>
+-------------------------------------------------------------------------------------------------->
     </main>
     <footer>
 
