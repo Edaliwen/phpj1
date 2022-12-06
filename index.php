4b9a71e74a4a4499e18f81c4eca38365d31a64d2
Edaliwen <amandine.valtat@gmail.com>
2022-12-06 16:54:46 +0100

    Update index.php
    Correction exercice 9

--- index.php
+++ index.php
@@ -396,6 +396,40 @@ Magnitude   |   Phrase
 Gérer tous les cas.  
 *Utiliser autre chose que des if else*</p>
 -------------------------------------------------------------------------------------------------->
+                <?php
+                    $magnitude = 0;
+                    switch($magnitude):
+                        case 1:
+                            echo "<p>Micro-séisme impossible à ressentir.</p><br>";
+                            break;
+                        case 2:
+                            echo "<p>Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.</p><br>";
+                            break;
+                        case 3:
+                            echo "<p>Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.</p><br>";
+                            break;
+                        case 4:
+                            echo "<p>Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.</p><br>";
+                            break;
+                        case 5:
+                            echo "<p>Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.</p><br>";
+                            break;
+                        case 6:
+                            echo "<p>Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.</p><br>";
+                            break;
+                        case 7:
+                            echo "<p>Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.</p><br>";
+                            break;
+                        case 8:
+                            echo "<p>Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.</p><br>";
+                            break;
+                        case 9:
+                            echo "<p>Séisme capable de tout détruire sur une très vaste zone.</p><br>";
+                            break;
+                        default: 
+                            echo "<p>Changez d'échelle, ou prenez un escabeau !</p><br>";
+                        endswitch;
+                ?>
     </main>
     <footer>
 
