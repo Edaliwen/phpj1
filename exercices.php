b7e8891e756242b407c5487f39f9d608334a7820
Edaliwen <amandine.valtat@gmail.com>
2022-12-07 16:27:01 +0100

    Update exercices.php
    Modif titre d'onglet

--- exercices.php
+++ exercices.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- notre css -->
     <link rel="stylesheet" href="assets/css/styleFront.css">
-    <title>PHP jour 1</title>
+    <title>PHP Exercices</title>
 </head>
 <body>
     <header class="container-fluid bg-info text-light py-2">
